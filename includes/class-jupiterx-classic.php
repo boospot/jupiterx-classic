da654841bcb6b0528b4fb6a4f7966e3aeea95e94
BooSpot <boospotmanager@gmail.com>
2019-01-17 21:04:09 +0500

    no need for admin style and scripts yet

--- includes/class-jupiterx-classic.php
+++ includes/class-jupiterx-classic.php
@@ -179,8 +179,8 @@ class Jupiterx_Classic {
 
 		$plugin_admin = new Jupiterx_Classic_Admin( $this->get_plugin_name(), $this->get_version() );
 
-		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_styles' );
-		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_scripts' );
+//		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_styles' );
+//		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_scripts' );
 
 	}
 
