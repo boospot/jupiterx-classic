c62c6381d650f53fe7accf1a6d3e66e1c8e9f6e6
BooSpot <boospotmanager@gmail.com>
2019-01-25 19:41:16 +0500

    fix: social icons not showing if single page is off

--- public/templates/archive/employee-card.php
+++ public/templates/archive/employee-card.php
@@ -11,10 +11,10 @@ $thumb_id = get_post_thumbnail_id();
 $is_single = get_post_meta( get_the_ID(), '_single_post', true );
 $is_single = ( $is_single == 'true' ) ? true : false;
 
-$job_title   = get_post_meta( get_the_ID(), '_position', true );
+$job_title        = get_post_meta( get_the_ID(), '_position', true );
 $description_text = get_post_meta( get_the_ID(), '_desc', true );
 
-$show_description = isset($description) && ($description == 'false') ? false : true;
+$show_description = isset( $description ) && ( $description == 'false' ) ? false : true;
 
 $email      = get_post_meta( get_the_ID(), '_email', true );
 $linkedin   = get_post_meta( get_the_ID(), '_linkedin', true );
@@ -22,7 +22,7 @@ $facebook   = get_post_meta( get_the_ID(), '_facebook', true );
 $twitter    = get_post_meta( get_the_ID(), '_twitter', true );
 $googleplus = get_post_meta( get_the_ID(), '_googleplus', true );
 $instagram  = get_post_meta( get_the_ID(), '_instagram', true );
-$output =  Jupiterx_Classic_Global::mk_employees_meta_information( get_the_ID());
+$output     = Jupiterx_Classic_Global::mk_employees_meta_information( get_the_ID() );
 
 
 //				var_dump( $is_single);
@@ -33,14 +33,14 @@ $output =  Jupiterx_Classic_Global::mk_employees_meta_information( get_the_ID())
 
 	if ( $is_single ){ ?>
 <link itemprop="sameAs" href="<?php echo get_the_permalink(); ?>"/>
-	<a href="<?php echo get_the_permalink(); ?>">
+    <a href="<?php echo get_the_permalink(); ?>">
 		<?php }
 		?>
-		<picture class="thumbnail">
+        <picture class="thumbnail">
 			<?php echo get_the_post_thumbnail( null, 'employees-large' ); ?>
-		</picture>
-		<div class="cpt-card-content">
-			<h2 itemprop="name"><?php echo get_the_title(); ?></h2>
+        </picture>
+        <div class="cpt-card-content">
+            <h2 itemprop="name"><?php echo get_the_title(); ?></h2>
 			<?php
 			if ( $job_title ) { ?>
                 <div itemprop="jobTitle"><?php echo $job_title; ?></div>
@@ -48,19 +48,17 @@ $output =  Jupiterx_Classic_Global::mk_employees_meta_information( get_the_ID())
 
 			<?php
 			if ( $show_description ) { ?>
-				<div itemprop="knowsAbout"><?php echo $description_text; ?></div>
+                <div itemprop="knowsAbout"><?php echo $description_text; ?></div>
 			<?php }
-//			var_export( $image_attributes );
-
+			//			var_export( $image_attributes );
 			?>
 
 
-
-		</div><!-- .card-content -->
+        </div><!-- .card-content -->
 		<?php
 		if ( $is_single ){ ?>
-	</a>
-	<div class="employee-social-links"><?php echo $output; ?></div>
-<?php }
-?>
+    </a>
+<?php } ?>
+    <div class="employee-social-links"><?php echo $output; ?></div>
+
 </article><!-- .card -->
